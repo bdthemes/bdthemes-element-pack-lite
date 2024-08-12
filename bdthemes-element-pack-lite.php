64a6094d744a69655b79e1b753308fd1e0553583
Shahidul Islam <bdkoder@gmail.com>
2024-08-12 16:35:21 +0600

    Notice fixed & DCI improved

--- bdthemes-element-pack-lite.php
+++ bdthemes-element-pack-lite.php
@@ -232,6 +232,10 @@ if ( ! element_pack_pro_installed() ) {
 					'is_premium'   => true,
 					'popup_notice'        => false,
 					'deactivate_feedback' => true,
+					'delay_time'   => [
+						// 'time' => 3 * DAY_IN_SECONDS,
+						'time' => 133 * MINUTE_IN_SECONDS,
+					],
 					'plugin_msg'   => '<p>Be Top-contributor by sharing non-sensitive plugin data and create an impact to the global WordPress community today! You can receive valuable emails periodically.</p>',
 				));
 			}
